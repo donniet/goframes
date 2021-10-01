32497a6e015c9721b3ecce52ba158dee69980179
Donnie Tolbert <donniet@gmail.com>
2021-10-01 11:02:40 -0500

    small fix for wind speed

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ const (
 )
 
 type SimpleFrame struct {
+	m         *model.Skyciv
 	Width     float64
 	Height    float64
 	Length    float64
@@ -44,14 +45,63 @@ type SimpleFrame struct {
 	RoofSnowLoad float64
 	RoofLiveLoad float64
 	RoofDeadLoad float64
-	WindLiveLoad float64
+	// WindLiveLoad float64
+	WindSpeed float64
+}
+
+func (f *SimpleFrame) Model() *model.Skyciv {
+	return f.m
+}
+
+func (f *SimpleFrame) Build() {
+	f.m = model.NewModel()
+
+	redPineGreen := f.m.NewMaterial("Red Pine (green)")
+	redPineGreen.Class = model.MaterialClassWood
+	redPineGreen.ElasticityModulus = 1280 // ksi
+	redPineGreen.Density = 25             // lb/ft^3
+	redPineGreen.PoissonsRatio = 0.27
+	redPineGreen.YieldStrength = 0.26   // ksi
+	redPineGreen.UltimateStrength = 0.3 // ksi
+
+	post := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
+	tie := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
+	rafter := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
+	brace := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "4 x 8")
+	plate := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
+
+	for z := 0.; z <= Length; z += Length / 2 {
+		f.bent(post, tie, rafter, brace, plate, z, Length/2)
+	}
+
+	f.roofAreaLoad(-f.RoofSnowLoad, "snow")
+	f.roofAreaLoad(-f.RoofDeadLoad, "dead")
+	f.roofAreaLoad(-f.RoofLiveLoad, "live")
+
+	f.windAreaLoad(f.WindSpeed, "wind")
+
+	sw := f.m.NewSelfWeight()
+	sw.LoadGroup = "SW1" // for some reason skyciv always uses SW1 for this...
+	sw.Y = -1
+
+	f.m.LoadCombinations.Mapping.DeadCases("dead", "SW1").LiveCases("live").SnowCases("snow")
+	f.m.LoadCombinations.Cases = []model.Case{
+		{Name: "ULS: 1. 1.4D", Dead: 1.4},
+		{Name: "ULS: 2. 1.2D + 1.6L + 0.5S", Dead: 1.2, Live: 1.6, Snow: 0.5},
+		{Name: "ULS: 3. 1.2D + 1.6S + L", Dead: 1.2, Snow: 1.6, Live: 1},
+		{Name: "ULS: 3. 1.2D + 1.6S + 0.5W", Dead: 1.2, Snow: 1.6, Wind: 0.5},
+		{Name: "ULS: 4. 1.2D + W + L + 0.5S", Dead: 1.2, Wind: 1, Live: 1, Snow: 0.5},
+		{Name: "ULS: 5. 1.2D + L + 0.2S", Dead: 1.2, Live: 1, Snow: 0.2},
+		{Name: "ULS: 6. 0.9D + W", Dead: 0.9, Wind: 1},
+	}
+
 }
 
 func WindPressure(airDensity, windSpeed float64) float64 {
 	return 0.5 * airDensity / Gravity * windSpeed * windSpeed
 }
 
-func windAreaLoad(m *model.Skyciv, windSpeed float64, loadGroup string) {
+func (f *SimpleFrame) windAreaLoad(windSpeed float64, loadGroup string) {
 	presureMag := WindPressure(AirDensity, windSpeed)
 
 	// sides
@@ -62,12 +112,12 @@ func windAreaLoad(m *model.Skyciv, windSpeed float64, loadGroup string) {
 
 		// left side of building
 		nl := []*model.Node{
-			m.FindNearestNode(-Width/2, 0, z),
-			m.FindNearestNode(-Width/2, Height, z),
-			m.FindNearestNode(-Width/2, Height, z+Length/2),
-			m.FindNearestNode(-Width/2, 0, z+Length/2),
+			f.m.FindNearestNode(-Width/2, 0, z),
+			f.m.FindNearestNode(-Width/2, Height, z),
+			f.m.FindNearestNode(-Width/2, Height, z+Length/2),
+			f.m.FindNearestNode(-Width/2, 0, z+Length/2),
 		}
-		if al, err := m.NewAreaLoad(nl); err != nil {
+		if al, err := f.m.NewAreaLoad(nl); err != nil {
 			panic(err)
 		} else {
 			al.LoadGroup = loadGroup
@@ -84,12 +134,12 @@ func windAreaLoad(m *model.Skyciv, windSpeed float64, loadGroup string) {
 
 		// left side of building
 		nl := []*model.Node{
-			m.FindNearestNode(-Width/2, Height, z),
-			m.FindNearestNode(0, roofTop, z),
-			m.FindNearestNode(0, roofTop, z+Length/4),
-			m.FindNearestNode(-Width/2, Height, z+Length/4),
+			f.m.FindNearestNode(-Width/2, Height, z),
+			f.m.FindNearestNode(0, roofTop, z),
+			f.m.FindNearestNode(0, roofTop, z+Length/4),
+			f.m.FindNearestNode(-Width/2, Height, z+Length/4),
 		}
-		if al, err := m.NewAreaLoad(nl); err != nil {
+		if al, err := f.m.NewAreaLoad(nl); err != nil {
 			panic(err)
 		} else {
 			al.LoadGroup = loadGroup
@@ -99,7 +149,7 @@ func windAreaLoad(m *model.Skyciv, windSpeed float64, loadGroup string) {
 	}
 }
 
-func roofAreaLoad(m *model.Skyciv, magnitude float64, loadGroup string) {
+func (f *SimpleFrame) roofAreaLoad(magnitude float64, loadGroup string) {
 	for i := 0; i < 4; i++ {
 		z := Length / 4 * float64(i)
 
@@ -107,13 +157,13 @@ func roofAreaLoad(m *model.Skyciv, magnitude float64, loadGroup string) {
 
 		// left side
 		nl := []*model.Node{
-			m.FindNearestNode(-Width/2, Height, z),
-			m.FindNearestNode(0, roofTop, z),
-			m.FindNearestNode(0, roofTop, z+Length/4),
-			m.FindNearestNode(-Width/2, Height, z+Length/4),
+			f.m.FindNearestNode(-Width/2, Height, z),
+			f.m.FindNearestNode(0, roofTop, z),
+			f.m.FindNearestNode(0, roofTop, z+Length/4),
+			f.m.FindNearestNode(-Width/2, Height, z+Length/4),
 		}
 
-		if al, err := m.NewAreaLoad(nl); err != nil {
+		if al, err := f.m.NewAreaLoad(nl); err != nil {
 			panic(err)
 		} else {
 			al.LoadGroup = loadGroup
@@ -123,13 +173,13 @@ func roofAreaLoad(m *model.Skyciv, magnitude float64, loadGroup string) {
 
 		// right side
 		nl = []*model.Node{
-			m.FindNearestNode(Width/2, Height, z),
-			m.FindNearestNode(0, roofTop, z),
-			m.FindNearestNode(0, roofTop, z+Length/4),
-			m.FindNearestNode(Width/2, Height, z+Length/4),
+			f.m.FindNearestNode(Width/2, Height, z),
+			f.m.FindNearestNode(0, roofTop, z),
+			f.m.FindNearestNode(0, roofTop, z+Length/4),
+			f.m.FindNearestNode(Width/2, Height, z+Length/4),
 		}
 
-		if al, err := m.NewAreaLoad(nl); err != nil {
+		if al, err := f.m.NewAreaLoad(nl); err != nil {
 			panic(err)
 		} else {
 			al.LoadGroup = loadGroup
@@ -139,8 +189,8 @@ func roofAreaLoad(m *model.Skyciv, magnitude float64, loadGroup string) {
 	}
 }
 
-func bent(m *model.Skyciv, post, tie, rafter, brace, plate *model.Section, z, betweenBents float64) {
-	first := len(m.Nodes) == 0
+func (f *SimpleFrame) bent(post, tie, rafter, brace, plate *model.Section, z, betweenBents float64) {
+	first := len(f.m.Nodes) == 0
 
 	post00 := post.NewContinuousMember(-Width/2, 0, z, -Width/2, Height, z)
 	post01 := post.NewContinuousMember(Width/2, 0, z, Width/2, Height, z)
@@ -151,7 +201,7 @@ func bent(m *model.Skyciv, post, tie, rafter, brace, plate *model.Section, z, be
 	post00.A().FixedSupport()
 	post01.A().FixedSupport()
 
-	rooftop := m.NewNode(0, Height+Width/2*RoofRise/RoofRun, z)
+	rooftop := f.m.NewNode(0, Height+Width/2*RoofRise/RoofRun, z)
 	rafter.NewContinuousMemberBetweenNodes(post00.B(), rooftop)
 	rafter.NewContinuousMemberBetweenNodes(post01.B(), rooftop)
 
@@ -186,15 +236,15 @@ func bent(m *model.Skyciv, post, tie, rafter, brace, plate *model.Section, z, be
 		return
 	}
 
-	rtt := m.NewNode(0, Height+Width/2*RoofRise/RoofRun, z-Length/4)
-	rtt0 := m.NewNode(-Width/2, Height, z-Length/4)
-	rtt1 := m.NewNode(Width/2, Height, z-Length/4)
+	rtt := f.m.NewNode(0, Height+Width/2*RoofRise/RoofRun, z-Length/4)
+	rtt0 := f.m.NewNode(-Width/2, Height, z-Length/4)
+	rtt1 := f.m.NewNode(Width/2, Height, z-Length/4)
 
 	rafter.NewContinuousMemberBetweenNodes(rtt0, rtt)
 	rafter.NewContinuousMemberBetweenNodes(rtt1, rtt)
 
-	topB0 := m.FindNearestNode(-Width/2, Height, z-betweenBents)
-	topB1 := m.FindNearestNode(Width/2, Height, z-betweenBents)
+	topB0 := f.m.FindNearestNode(-Width/2, Height, z-betweenBents)
+	topB1 := f.m.FindNearestNode(Width/2, Height, z-betweenBents)
 
 	// connect with plates
 	p00 := plate.NewContinuousMemberBetweenNodes(topA0, rtt0)
@@ -207,9 +257,9 @@ func bent(m *model.Skyciv, post, tie, rafter, brace, plate *model.Section, z, be
 		panic(err)
 	} else if pb1, _, err := p10.Split(BraceRise); err != nil {
 		panic(err)
-	} else if postAttach0, err := m.FindNearestMemberAndSplitAt(pb0.X, pb0.Y-BraceRise, pb0.Z+BraceRise); err != nil {
+	} else if postAttach0, err := f.m.FindNearestMemberAndSplitAt(pb0.X, pb0.Y-BraceRise, pb0.Z+BraceRise); err != nil {
 		panic(fmt.Sprintf("could not find the post attachment for brace: %e", err))
-	} else if postAttach1, err := m.FindNearestMemberAndSplitAt(pb1.X, pb1.Y-BraceRise, pb1.Z+BraceRise); err != nil {
+	} else if postAttach1, err := f.m.FindNearestMemberAndSplitAt(pb1.X, pb1.Y-BraceRise, pb1.Z+BraceRise); err != nil {
 		panic(fmt.Sprintf("could not find the post attachment for brace, %e", err))
 	} else {
 		brace.NewContinuousMemberBetweenNodes(pb0, postAttach0)
@@ -220,9 +270,9 @@ func bent(m *model.Skyciv, post, tie, rafter, brace, plate *model.Section, z, be
 		panic(err)
 	} else if pb1, _, err := p11.Split(BraceRise); err != nil {
 		panic(err)
-	} else if postAttach0, err := m.FindNearestMemberAndSplitAt(pb0.X, pb0.Y-BraceRise, pb0.Z-BraceRise); err != nil {
+	} else if postAttach0, err := f.m.FindNearestMemberAndSplitAt(pb0.X, pb0.Y-BraceRise, pb0.Z-BraceRise); err != nil {
 		panic(fmt.Sprintf("could not find the post attachment for brace: %e", err))
-	} else if postAttach1, err := m.FindNearestMemberAndSplitAt(pb1.X, pb1.Y-BraceRise, pb1.Z-BraceRise); err != nil {
+	} else if postAttach1, err := f.m.FindNearestMemberAndSplitAt(pb1.X, pb1.Y-BraceRise, pb1.Z-BraceRise); err != nil {
 		panic(fmt.Sprintf("could not find the post attachment for brace, %e", err))
 	} else {
 		brace.NewContinuousMemberBetweenNodes(pb0, postAttach0)
@@ -232,48 +282,23 @@ func bent(m *model.Skyciv, post, tie, rafter, brace, plate *model.Section, z, be
 }
 
 func main() {
-	m := model.NewModel()
-
-	redPineGreen := m.NewMaterial("Red Pine (green)")
-	redPineGreen.Class = model.MaterialClassWood
-	redPineGreen.ElasticityModulus = 1280 // ksi
-	redPineGreen.Density = 25             // lb/ft^3
-	redPineGreen.PoissonsRatio = 0.27
-	redPineGreen.YieldStrength = 0.26   // ksi
-	redPineGreen.UltimateStrength = 0.3 // ksi
-
-	post := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
-	tie := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
-	rafter := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
-	brace := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "4 x 8")
-	plate := redPineGreen.NewSectionFromLibrary("American", "NDS", "Sawn Lumber", "8 x 10")
-
-	for z := 0.; z <= Length; z += Length / 2 {
-		bent(m, post, tie, rafter, brace, plate, z, Length/2)
+	f := &SimpleFrame{
+		Width:        Width,
+		Height:       Height,
+		Length:       Length,
+		TieHeight:    TieHeight,
+		BraceRise:    BraceRise,
+		RoofRise:     RoofRise,
+		RoofRun:      RoofRun,
+		Bents:        Bents,
+		RoofSnowLoad: RoofSnowLoad,
+		RoofLiveLoad: RoofLiveLoad,
+		RoofDeadLoad: RoofDeadLoad,
+		WindSpeed:    WindSpeed,
 	}
+	f.Build()
 
-	roofAreaLoad(m, -RoofSnowLoad, "snow")
-	roofAreaLoad(m, -RoofDeadLoad, "dead")
-	roofAreaLoad(m, -RoofLiveLoad, "live")
-
-	windAreaLoad(m, WindSpeed, "wind")
-
-	sw := m.NewSelfWeight()
-	sw.LoadGroup = "SW1" // for some reason skyciv always uses SW1 for this...
-	sw.Y = -1
-
-	m.LoadCombinations.Mapping.DeadCases("dead", "SW1").LiveCases("live").SnowCases("snow")
-	m.LoadCombinations.Cases = []model.Case{
-		{Name: "ULS: 1. 1.4D", Dead: 1.4},
-		{Name: "ULS: 2. 1.2D + 1.6L + 0.5S", Dead: 1.2, Live: 1.6, Snow: 0.5},
-		{Name: "ULS: 3. 1.2D + 1.6S + L", Dead: 1.2, Snow: 1.6, Live: 1},
-		{Name: "ULS: 3. 1.2D + 1.6S + 0.5W", Dead: 1.2, Snow: 1.6, Wind: 0.5},
-		{Name: "ULS: 4. 1.2D + W + L + 0.5S", Dead: 1.2, Wind: 1, Live: 1, Snow: 0.5},
-		{Name: "ULS: 5. 1.2D + L + 0.2S", Dead: 1.2, Live: 1, Snow: 0.2},
-		{Name: "ULS: 6. 0.9D + W", Dead: 0.9, Wind: 1},
-	}
-
-	b, err := json.MarshalIndent(m, "", "\t")
+	b, err := json.MarshalIndent(f.Model(), "", "\t")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%e", err)
 		return
